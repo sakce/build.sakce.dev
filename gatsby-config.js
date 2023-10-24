97794ac1aadb8a700aed65552d341fa8a7191b86
sakce <jovan@tasman.ai>
2023-10-25 00:20:53 +0100

    feat: init Gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `build-in-public`,
+    siteUrl: `https://build.sakce.dev`,
+  },
+  plugins: [],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+const IndexPage = () => {
+  return (
+    <main>
+      <h1>Welcome to build.sakce.dev! This is a WIP. 🚀</h1>
+    </main>
+  )
+}
+
+export const Head = () => <title>Home</title>
+
+export default IndexPage
