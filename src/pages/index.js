2f7583e84cd808e9e96e63408c8eec8a7257178a
Jovan Sakovic <49978945+sakce@users.noreply.github.com>
2023-11-07 22:49:31 +0000

    fix: un-href the eyes emoji

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => {
         Welcome to <span style={{ color: '#6B7FD7' }}>build.sakce.dev</span>! This is a WIP. 🚀
       </h1>
       <h2 className={ h1 }>
-        <a href="https://github.com/sakce/build.sakce.dev">Take a sneak peek. 👀 </a>
+        <a href="https://github.com/sakce/build.sakce.dev">Take a sneak peek</a>. 👀
       </h2>
     </main>
   );
